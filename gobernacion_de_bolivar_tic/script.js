ec63b990c01a5973dea6b5c4006b5694c7969537
jfpn1920cr <162055100+jfpn1920cg@users.noreply.github.com>
2024-03-26 17:02:18 -0500

    Agregando_nuevo_contenido

--- gobernacion_de_bolivar_tic/script.js
+++ gobernacion_de_bolivar_tic/script.js
@@ -46,83 +46,105 @@ indicators.forEach((indicator, index) => {
     });
 });
 startAutoSlide();
-//funcionalidad_zona_de_sugerencia
+//funcionalidad_zona_de_sugerencia_parte_1
 function visitPage(url) {
   window.location.href = url;
 }
-//funcionalidad_carrusel_parte_2
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector(".carousel");
-    const slides = document.querySelectorAll(".slide");
-    const indicatorsContainer = document.querySelector(".indicators");
-    const prevButton = document.querySelector(".prev-button");
-    const nextButton = document.querySelector(".next-button");
-
-    let currentIndex = 0;
-    let intervalId;
-
-    // Crear indicadores
-    slides.forEach((slide, index) => {
-        const indicator = document.createElement("div");
-        indicator.classList.add("indicator");
-        indicatorsContainer.appendChild(indicator);
-
-        indicator.addEventListener("click", () => {
-            goToSlide(index);
+//funcionalidad_zona_de_sugerencia_parte_2
+const categories = [
+    {
+        image: 'imagen_9.png',
+        title: 'Ruralmente digital: Desarrolla tu inclusión digital',
+        description: 'Descubre cómo usar las TIC para transformar tu vida y la de tu comunidad en el campo. Conoce herramientas para conectarte, comunicarte con el mundo y mejorar las actividades y procesos agropecuarios desde tu hogar.'
+    },
+    {
+        image: 'imagen_10.png',
+        title: 'Navegando Juntos: Formación en Internet para personas mayores en el mundo rural.',
+        description: 'Descubre el potencial de las TIC para el campo, conecta con nuevas oportunidades y comparte tus tradiciones con las generaciones futuras. ¡Prepárate para una aventura digital!'
+    },
+    {
+        image: 'imagen_11.png',
+        title: 'Campo conectado, desarrollo socioeconómico a través de habilidades digitales',
+        description: 'Explora las raíces y la identidad de tu comunidad con ayuda de las herramientas digitales, muéstrale al mundo lo que haces y conoce los beneficios del mundo virtual para comercializar tus productos y mejorar tus ingresos.'
+    },
+    {
+        image: 'imagen_12.png',
+        title: 'Formación en Internet para personas mayores',
+        description: '¡El ratón no muerde! Descubre el Internet paso a paso. Desde los aspectos más básicos, como las partes de un computador hasta la navegación. Viaja, infórmate y comunícate con los tuyos.'
+    },
+    {
+        image: 'imagen_13.png',
+        title: 'Introducción al mundo digital',
+        description: 'Aprende a navegar en Internet desde cero y úsalo a tu medida, ya sea para comunicarte, trabajar, informarte, estudiar, ampliar las fronteras de tu negocio o entretenerte.'
+    },
+    {
+        image: 'imagen_14.png',
+        title: 'Soy un profe TIC: Comparte con el mundo tus conocimientos TIC',
+        description: 'Utiliza Internet y las tecnologías como instrumentos de transformación social, aprendiendo los primeros pasos para enseñar a tu comunidad a usar estas herramientas'
+    },
+    {
+        image: 'imagen_15.png',
+        title: 'Sácale provecho a tus dispositivos móviles: celulares, tabletas y computadores portátiles',
+        description: 'Aprende cómo sacarle provecho al celular, las tabletas o los computadores portátiles, de acuerdo a tus necesidades. Puedes vender, crear contenidos, ayudar a tu comunidad y más.'
+    },
+    {
+        image: 'imagen_16.png',
+        title: 'Transforma tu mundo con internet: paso a paso del activismo digital',
+        description: 'Aprende a utilizar Internet como motor de oportunidades y cambios positivos para tu comunidad. Conoce herramientas para la educación y movilización social en Internet.'
+    },
+    {
+        image: 'imagen_17.png',
+        title: 'Ciberperiodismo comunitario a tu alcance',
+        description: 'Conoce la importancia de los medios digitales con enfoque comunitario y aprende claves y herramientas de diseño, edición de audio y video para ejercer el ciberperiodismo.'
+    },
+    {
+        image: 'imagen_18.png',
+        title: 'Las TIC aliadas fundamentales para el teletrabajo, trabajo en casa y trabajo remoto',
+        description: 'Aprende todo lo que necesitas sobre trabajar en casa, o dónde quieras, desde cómo gestionar tu propio tiempo hasta qué herramientas digitales utilizar.'
+    },
+    {
+        image: 'imagen_19.png',
+        title: 'Aprende a usar internet fácilmente',
+        description: 'Aprende a utilizar herramientas, plataformas y aplicaciones en tu vida diaria para comunicarte con el mundo. Internet te ofrece oportunidades infinitas. ¡Desarrolla tus habilidades según tus necesidades y aprovecha al máximo esta nueva era digital!'
+    },
+    {
+        image: 'imagen_20.png',
+        title: 'Cómo hacer trámites por internet con el estado',
+        description: 'Ahorra tiempo y conoce cómo comunicarte con las entidades del Estado. Aprende sobre el gobierno digital, el Sistema único de Información de Trámites y Servicios y mucho más.'
+    },
+    {
+        image: 'imagen_21.png',
+        title: 'Aprende a cuidarte en el mundo digital',
+        description: 'Conoce cómo cuidar tus datos personales, evita estafas y suplantaciones de identidad, aprende sobre salud digital y ciberadicción, y navega tranquilo.'
+    },
+];
+function createCategories() {
+    const categoriesArea = document.getElementById('categoriesArea');
+    categories.forEach(category => {
+        const categoryElement = document.createElement('div');
+        categoryElement.classList.add('category');
+        const imageElement = document.createElement('img');
+        imageElement.src = category.image;
+        const titleElement = document.createElement('div');
+        titleElement.classList.add('category-title');
+        titleElement.textContent = category.title;
+        const descriptionElement = document.createElement('div');
+        descriptionElement.classList.add('category-description');
+        descriptionElement.textContent = category.description;
+        const buttonElement = document.createElement('button');
+        buttonElement.textContent = 'Inicia con Tic';
+        buttonElement.addEventListener('click', () => {
+            console.log('Iniciar con TIC en ' + category.title);
         });
+        categoryElement.appendChild(imageElement);
+        categoryElement.appendChild(titleElement);
+        categoryElement.appendChild(descriptionElement);
+        categoryElement.appendChild(buttonElement);
+        categoriesArea.appendChild(categoryElement);
     });
-
-    const indicators = document.querySelectorAll(".indicator");
-
-    // Función para avanzar al siguiente slide
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        goToSlide(currentIndex);
-    }
-
-    // Función para retroceder al slide anterior
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        goToSlide(currentIndex);
-    }
-
-    // Función para ir a un slide específico
-    function goToSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.style.transform = `translateX(-${index * 100}%)`;
-            indicators[i].classList.remove("active");
-        });
-
-        indicators[index].classList.add("active");
-        currentIndex = index;
-    }
-
-    // Iniciar el carrusel automáticamente
-    function startCarousel() {
-        intervalId = setInterval(nextSlide, 3000); // Cambia de slide cada 3 segundos
-    }
-
-    // Detener el carrusel cuando el usuario interactúa con él
-    function stopCarousel() {
-        clearInterval(intervalId);
-    }
-
-    // Ir al slide anterior al hacer clic en el botón previo
-    prevButton.addEventListener("click", () => {
-        stopCarousel();
-        prevSlide();
-    });
-
-    // Ir al siguiente slide al hacer clic en el botón siguiente
-    nextButton.addEventListener("click", () => {
-        stopCarousel();
-        nextSlide();
-    });
-
-    // Iniciar el carrusel
-    startCarousel();
-
-    // Detener y reanudar el carrusel al pasar el mouse
-    carousel.addEventListener("mouseenter", stopCarousel);
-    carousel.addEventListener("mouseleave", startCarousel);
+}
+document.addEventListener('DOMContentLoaded', createCategories);
+//funcionalidad_publicidad
+document.getElementById("image-22").addEventListener("click", function() {
+    window.location.href = "https://www.ejemplo.com/pagina"; // Cambia la URL a la que deseas dirigir al usuario
 });
